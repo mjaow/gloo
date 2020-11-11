5df910d5f5105c00e7416675619e741d6bba14a0
EItanya <eitan.yarmush@solo.io>
2020-11-11 11:07:21 -0500

    compiles

--- projects/gloo/pkg/setup/setup.go
+++ projects/gloo/pkg/setup/setup.go
@@ -2,15 +2,9 @@ package setup
 
 import (
 	"context"
-	"os"
 
 	"github.com/solo-io/gloo/pkg/version"
 
-	"go.uber.org/zap"
-
-	"github.com/solo-io/gloo/pkg/utils/usage"
-	"github.com/solo-io/gloo/projects/metrics/pkg/metricsservice"
-	"github.com/solo-io/go-utils/contextutils"
 	"github.com/solo-io/reporting-client/pkg/client"
 
 	"github.com/solo-io/gloo/pkg/utils/setuputils"
@@ -18,15 +12,8 @@ import (
 )
 
 func Main(customCtx context.Context) error {
-	var usageReporter client.UsagePayloadReader
-	metricsStorage, err := metricsservice.NewDefaultConfigMapStorage(os.Getenv("POD_NAMESPACE"))
-	if err != nil {
-		contextutils.LoggerFrom(customCtx).Warnw("Could not create metrics storage loader - will not report usage: %s", zap.Error(err))
-	} else {
-		usageReporter = &usage.DefaultUsageReader{MetricsStorage: metricsStorage}
-	}
-
-	return startSetupLoop(customCtx, usageReporter)
+	// TODO: Figure out if we want to recreate usage reporter without the envoy metrics service
+	return startSetupLoop(customCtx, nil)
 }
 
 func StartGlooInTest(customCtx context.Context) error {
