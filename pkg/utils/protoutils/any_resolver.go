1081aa8603f117a810b3c2d7fbab37739a8fb875
EItanya <eitan.yarmush@solo.io>
2020-11-11 13:56:15 -0500

    added comments

--- pkg/utils/protoutils/any_resolver.go
+++ pkg/utils/protoutils/any_resolver.go
@@ -12,6 +12,7 @@ import (
 type MultiAnyResolver struct{}
 
 func (m *MultiAnyResolver) Resolve(typeUrl string) (proto.Message, error) {
+	// Mostly copy pasted from: https://github.com/golang/protobuf/blob/84668698ea25b64748563aa20726db66a6b8d299/jsonpb/jsonpb.go#L93
 	messageType := typeUrl
 	if slash := strings.LastIndex(typeUrl, "/"); slash >= 0 {
 		messageType = messageType[slash+1:]
@@ -19,12 +20,13 @@ func (m *MultiAnyResolver) Resolve(typeUrl string) (proto.Message, error) {
 	var mt reflect.Type
 	mt = proto.MessageType(messageType)
 	if mt == nil {
+		// If no any exists for golang proto, check gogo proto
 		mt = proto2.MessageType(messageType)
 		if mt == nil {
+			// If neither exists, there is an error
 			return nil, eris.Errorf("unknown message type %q", messageType)
 		}
 	}
 	return reflect.New(mt.Elem()).Interface().(proto.Message), nil
-
 }
 
