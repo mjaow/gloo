6a16e50fd10930c55e435a4a4ca0a7d055eb0e9c
Sam Heilbron <samheilbron@gmail.com>
2020-11-11 23:05:25 -0500

    Further updates to zipkin e2e test

--- test/e2e/zipkin_test.go
+++ test/e2e/zipkin_test.go
@@ -53,14 +53,20 @@ var _ =Describe("Zipkin config loading", func() {
 		cancel()
 	})
 
-	startZipkinServer := func() {
+
+
+	startZipkinServer := func(handler http.Handler) {
 		zipkinServer = &http.Server{
 			Addr: ":9411",
-			Handler: nil,
+			Handler: handler,
 		}
 		go func() {
 			zipkinServer.ListenAndServe()
 		}()
+	}
+
+	startZipkinServerWithDefaultHandler := func() {
+		startZipkinServer(nil)
 	}
 
 	stopZipkinServer := func() {
@@ -104,7 +110,7 @@ var _ =Describe("Zipkin config loading", func() {
 			fmt.Fprintf(w, "Dummy Zipkin Collector received request on - %q", html.EscapeString(r.URL.Path))
 			apiHit <- true
 		})
-		startZipkinServer()
+		startZipkinServerWithDefaultHandler()
 
 		testRequest := basicReq()
 		Eventually(testRequest, 15, 1).Should(ContainSubstring(`<title>Envoy Admin</title>`))
@@ -130,34 +136,34 @@ var _ =Describe("Zipkin config loading", func() {
 			writeNamespace string
 		)
 
-		//basicReq := func() func() (string, error) {
-		//	return func() (string, error) {
-		//		req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:%d/", "localhost", defaults.HttpPort), nil)
-		//		if err != nil {
-		//			return "", err
-		//		}
-		//		req.Header.Set("Content-Type", "application/json")
-		//
-		//		// Set a random trace ID
-		//		req.Header.Set("x-client-trace-id", "test-trace-id-1234567890")
-		//
-		//		res, err := http.DefaultClient.Do(req)
-		//		if err != nil {
-		//			return "", err
-		//		}
-		//		defer res.Body.Close()
-		//		body, err := ioutil.ReadAll(res.Body)
-		//		return string(body), err
-		//	}
-		//}
+		basicReq := func() func() (string, error) {
+			return func() (string, error) {
+				port := defaults.HttpPort
+				req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:%d/", "127.0.0.1", port), nil)
+				if err != nil {
+					return "", err
+				}
+				req.Header.Set("Content-Type", "application/json")
+
+				// Set a random trace ID
+				req.Header.Set("x-client-trace-id", "test-trace-id-1234567890")
+
+				res, err := http.DefaultClient.Do(req)
+				if err != nil {
+					return "", err
+				}
+				defer res.Body.Close()
+				body, err := ioutil.ReadAll(res.Body)
+				return string(body), err
+			}
+		}
 
 		BeforeEach(func() {
 			ctx, cancel = context.WithCancel(context.Background())
 			defaults.HttpPort = services.NextBindPort()
 			defaults.HttpsPort = services.NextBindPort()
 
-
-
+			// run gloo
 			writeNamespace = "gloo-system"
 			ro := &services.RunOptions{
 				NsToWrite: writeNamespace,
@@ -167,22 +173,22 @@ var _ =Describe("Zipkin config loading", func() {
 					DisableUds: true,
 				},
 			}
-
 			testClients = services.RunGlooGatewayUdsFds(ctx, ro)
+
+			// write gateways and wait for them to be created
 			err := gloohelpers.WriteDefaultGateways(writeNamespace, testClients.GatewayClient)
 			Expect(err).NotTo(HaveOccurred(), "Should be able to write default gateways")
-
-			// wait for the two gateways to be created.
 			Eventually(func() (gatewayv1.GatewayList, error) {
-				gateways, err := testClients.GatewayClient.List(writeNamespace, clients.ListOpts{})
-
-				fmt.Printf("%+v\n", gateways[0])
-				return gateways, err
+				return testClients.GatewayClient.List(writeNamespace, clients.ListOpts{})
 			}, "10s", "0.1s").Should(HaveLen(2), "Gateways should be present")
+
+			// run envoy
+			err = envoyInstance.RunWithRole(writeNamespace+"~"+gatewaydefaults.GatewayProxyName, testClients.GlooPort)
+			Expect(err).NotTo(HaveOccurred())
 		})
 
 		AfterEach(func() {
-			// envoyInstance.Clean()
+			envoyInstance.Clean()
 			cancel()
 		})
 
@@ -197,8 +203,6 @@ var _ =Describe("Zipkin config loading", func() {
 		}
 
 		It("should not send trace msgs with nil provider", func() {
-			envoyInstance.Run(testClients.GlooPort)
-
 			gatewayClient := testClients.GatewayClient
 			gw, err := gatewayClient.List(writeNamespace, clients.ListOpts{})
 			Expect(err).NotTo(HaveOccurred())
@@ -214,44 +218,78 @@ var _ =Describe("Zipkin config loading", func() {
 				Expect(err).NotTo(HaveOccurred())
 			}
 
-			// write a virtual service so we have a proxy
-			vs := getTrivialVirtualServiceForUpstream("gloo-system", core.ResourceRef{Name: "test", Namespace: "test"})
-			_, err = testClients.VirtualServiceClient.Write(vs, clients.WriteOpts{})
+			// create test upstream
+			// this is the upstream that will handle requests with tracing enabled
+			testUs := v1helpers.NewTestHttpUpstream(ctx, envoyInstance.LocalAddr())
+			_, err = testClients.UpstreamClient.Write(testUs.Upstream, clients.WriteOpts{})
 			Expect(err).NotTo(HaveOccurred())
 
-			// make sure it propagates to proxy
+			// create zipkin upstream
+			zipkinUs := &gloov1.Upstream{
+				Metadata: core.Metadata{
+					Name: 		"zipkin",
+					Namespace: 	"default",
+				},
+				UpstreamType: &gloov1.Upstream_Static{
+					Static: &static_plugin_gloo.UpstreamSpec{
+						Hosts: []*static_plugin_gloo.Host{
+							{
+								Addr: "127.0.0.1",
+								Port: 9411,
+							},
+						},
+					},
+				},
+			}
+			_, err = testClients.UpstreamClient.Write(zipkinUs, clients.WriteOpts{})
+			Expect(err).NotTo(HaveOccurred())
+
+			// write a virtual service so we have a proxy to our test upstream
+			testVs := getTrivialVirtualServiceForUpstream("gloo-system", testUs.Upstream.Metadata.Ref())
+			_, err = testClients.VirtualServiceClient.Write(testVs, clients.WriteOpts{})
+			Expect(err).NotTo(HaveOccurred())
+
+			// ensure the proxy and virtual service are created
 			Eventually(
-				func() (int, error) {
-					numProvider := 0
-					proxy, err := testClients.ProxyClient.Read(writeNamespace, gatewaydefaults.GatewayProxyName, clients.ReadOpts{})
-					if err != nil {
-						return 0, err
-					}
-					for _, l := range proxy.Listeners {
-						if h := l.GetHttpListener(); h != nil {
-							if p := h.GetOptions(); p != nil {
-								if hcmSettings := p.GetHttpConnectionManagerSettings(); hcmSettings != nil {
-									if tracing := hcmSettings.GetTracing(); tracing != nil {
-										if tracing.Provider != nil {
-											numProvider++
-										}
-									}
-								}
-							}
-						}
-					}
-					return numProvider, nil
-				}, "5s", "0.1s").Should(Equal(0))
+				func() (*gloov1.Proxy, error) {
+					return testClients.ProxyClient.Read(writeNamespace, gatewaydefaults.GatewayProxyName, clients.ReadOpts{})
+				}, "5s", "0.1s").ShouldNot(BeNil())
+			Eventually(
+				func() (*gatewayv1.VirtualService, error) {
+					return testClients.VirtualServiceClient.Read(testVs.Metadata.Namespace, testVs.Metadata.Name, clients.ReadOpts{})
+				}, "5s", "0.1s").ShouldNot(BeNil())
+
+			// ensure the upstream is reachable
+			TestUpstreamReachable := func() {
+				v1helpers.TestUpstreamReachable(defaults.HttpPort, testUs, nil)
+			}
+			TestUpstreamReachable()
+
+			// Start a dummy server listening on 9411 for Zipkin requests
+			apiHit := make(chan bool, 1)
+			zipkinHandler := http.NewServeMux()
+			zipkinHandler.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				Expect(r.URL.Path).To(Equal("/api/v2/spans")) // Zipkin json collector API
+				fmt.Fprintf(w, "Dummy Zipkin Collector received request on - %q", html.EscapeString(r.URL.Path))
+				apiHit <- true
+			}))
+			startZipkinServer(zipkinHandler)
+
+			testRequest := basicReq()
+			Eventually(testRequest, 15*time.Second, 1*time.Second).Should(BeEmpty())
+
+			// we haven't configured tracing, so we don't expect the zipkin server to receive an api hit
+			Eventually(apiHit, 5*time.Second).Should(Not(Receive()))
+
+			stopZipkinServer()
 		})
 
-		FIt("should send trace msgs with zipkin provider", func() {
-			err := envoyInstance.RunWithRole(writeNamespace+"~"+gatewaydefaults.GatewayProxyName, testClients.GlooPort)
-			Expect(err).NotTo(HaveOccurred())
-
+		It("should send trace msgs with zipkin provider", func() {
 			gatewayClient := testClients.GatewayClient
 			gw, err := gatewayClient.List(writeNamespace, clients.ListOpts{})
 			Expect(err).NotTo(HaveOccurred())
 
+			// configure zipkin, and write tracing configuration to gateway
 			zipkinConfig := &envoy_config_trace_v3.ZipkinConfig{
 				CollectorCluster:         "zipkin_default",
 				CollectorEndpoint:        "/api/v2/spans",
@@ -273,69 +311,73 @@ var _ =Describe("Zipkin config loading", func() {
 			for _, g := range gw {
 				httpGateway := g.GetHttpGateway()
 				setTracingOnGateway(httpGateway, zipkinTracing)
+
 				_, err := gatewayClient.Write(g, clients.WriteOpts{Ctx: ctx, OverwriteExisting: true})
 				Expect(err).NotTo(HaveOccurred())
 			}
 
-			// write a virtual service so we have a proxy
-			testUpstream := v1helpers.NewTestHttpUpstream(ctx, envoyInstance.LocalAddr())
-			_, err = testClients.UpstreamClient.Write(testUpstream.Upstream, clients.WriteOpts{})
+			// create test upstream
+			// this is the upstream that will handle requests with tracing enabled
+			testUs := v1helpers.NewTestHttpUpstream(ctx, envoyInstance.LocalAddr())
+			_, err = testClients.UpstreamClient.Write(testUs.Upstream, clients.WriteOpts{})
 			Expect(err).NotTo(HaveOccurred())
 
+			// create zipkin upstream
 			zipkinUs := &gloov1.Upstream{
-				Metadata:                    core.Metadata{
-					Name:                 "zipkin",
-					Namespace:            "default",
+				Metadata: core.Metadata{
+					Name: 		"zipkin",
+					Namespace: 	"default",
 				},
 				UpstreamType: &gloov1.Upstream_Static{
 					Static: &static_plugin_gloo.UpstreamSpec{
 						Hosts: []*static_plugin_gloo.Host{
 							{
-								Addr:                 "127.0.0.1",
-								Port:                 9411,
+								Addr: "127.0.0.1",
+								Port: 9411,
 							},
 						},
 					},
 				},
 			}
-			_, err = testClients.UpstreamClient.Write(zipkinUs, clients.WriteOpts{})
+			 _, err = testClients.UpstreamClient.Write(zipkinUs, clients.WriteOpts{})
 			Expect(err).NotTo(HaveOccurred())
 
-			vs := getTrivialVirtualServiceForUpstream("gloo-system", testUpstream.Upstream.Metadata.Ref())
-			_, err = testClients.VirtualServiceClient.Write(vs, clients.WriteOpts{})
+			// write a virtual service so we have a proxy to our test upstream
+			testVs := getTrivialVirtualServiceForUpstream("gloo-system", testUs.Upstream.Metadata.Ref())
+			_, err = testClients.VirtualServiceClient.Write(testVs, clients.WriteOpts{})
 			Expect(err).NotTo(HaveOccurred())
 
-
-			// make sure it propagates to proxy
+			// ensure the proxy and virtual service are created
 			Eventually(
 				func() (*gloov1.Proxy, error) {
 					return testClients.ProxyClient.Read(writeNamespace, gatewaydefaults.GatewayProxyName, clients.ReadOpts{})
 				}, "5s", "0.1s").ShouldNot(BeNil())
+			Eventually(
+				func() (*gatewayv1.VirtualService, error) {
+					return testClients.VirtualServiceClient.Read(testVs.Metadata.Namespace, testVs.Metadata.Name, clients.ReadOpts{})
+				}, "5s", "0.1s").ShouldNot(BeNil())
 
+			// ensure the upstream is reachable
 			TestUpstreamReachable := func() {
-				v1helpers.TestUpstreamReachable(defaults.HttpPort, testUpstream, nil)
+				v1helpers.TestUpstreamReachable(defaults.HttpPort, testUs, nil)
 			}
-
 			TestUpstreamReachable()
 
-
-
-			apiHit := make(chan bool, 1)
-
 			// Start a dummy server listening on 9411 for Zipkin requests
-			mux := http.NewServeMux()
-			mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			apiHit := make(chan bool, 1)
+			zipkinHandler := http.NewServeMux()
+			zipkinHandler.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				Expect(r.URL.Path).To(Equal("/api/v2/spans")) // Zipkin json collector API
 				fmt.Fprintf(w, "Dummy Zipkin Collector received request on - %q", html.EscapeString(r.URL.Path))
 				apiHit <- true
-			})
-			startZipkinServer()
+			}))
+			startZipkinServer(zipkinHandler)
 
-			//testRequest := basicReq()
-			//Eventually(testRequest, "15s", "1s").Should(ContainSubstring(`<title>Envoy Admin</title>`))
+			testRequest := basicReq()
+			Eventually(testRequest, 15*time.Second, 1*time.Second).Should(BeEmpty())
 
-			truez := true
-			Eventually(apiHit, 5*time.Second).Should(Receive(&truez))
+			expectedZipkinApiHit := true
+			Eventually(apiHit, 5*time.Second).Should(Receive(&expectedZipkinApiHit))
 
 			stopZipkinServer()
 		})
